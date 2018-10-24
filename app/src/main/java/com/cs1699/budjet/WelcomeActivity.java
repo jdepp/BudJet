6fbc350ca6e3a9d33ce88faad76af791ae874ec0
Jeremy Deppen <jeremydeppen@fq-wifi-10-215-205-145.wireless.pitt.edu>
2018-10-24 13:19:43 -0400

    Pushing inital project

---
+++ app/src/main/java/com/cs1699/budjet/WelcomeActivity.java
@@ -0,0 +1,35 @@
+package com.cs1699.budjet;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private final Context mContext = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void onLoginClicked(View view) {
+        Toast.makeText(mContext, "Login clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void onRegisterClicked(View view) {
+        Toast.makeText(mContext, "Register clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void onForgotPasswordClicked(View view) {
+        Toast.makeText(mContext, "Forgot password clicked", Toast.LENGTH_LONG).show();
+    }
+
+}
